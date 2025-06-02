dbcfca6b7f80e184d1705609b5470adee489dd3f
Meshack Millan <millanmeshack96@gmail.com>
2025-06-02 10:17:51 +0300

    Add files via upload
    groupby order by and having

--- groupbyorderby.sql
+++ groupbyorderby.sql
@@ -20,6 +20,22 @@ select gender,age
 from employee_demographics
 order by age desc;
 
+select gender,avg(age)
+from employee_demographics
+group by gender
+having avg(age)>40;
 
+select occupation, avg(salary),count(occupation),max(salary)
+from employee_salary
+where occupation like ("%manager%")
+group by occupation
+having avg(salary)>60000;
+
+select * from employee_demographics;
+
+select gender,avg(age)
+from employee_demographics
+group by gender
+having avg(age)>40;
 
 
